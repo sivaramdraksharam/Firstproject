9ba005c3499278e56058a67774abaf290bdfce7f
Sivaram Prasad <35053169+sivaramdraksharam@users.noreply.github.com>
2022-09-30 11:50:36 +0530

    Manager bean adding
    put Manager class in beans package

---
+++ Manager.java
@@ -0,0 +1,20 @@
+package com.satyam.day3.beans;
+
+public class Manager extends Employee{
+	
+	private float conveyance;
+	
+	public Manager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public float getConveyance() {
+		return conveyance;
+	}
+
+	public void setConveyance(float conveyance) {
+		this.conveyance = conveyance;
+	}
+	
+
+}
